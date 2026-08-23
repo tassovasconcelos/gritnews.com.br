69e25866963f9c99e0162925bce64bd16774844a
Tasso Sousa de Vasconcelos Júnior <tassovasconcelos@gmail.com>
2026-08-23 09:15:27 -0300

    feat(sr-padeiro): initialize marketing tracking

--- apps/sr-padeiro/src/main.tsx
+++ apps/sr-padeiro/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initTracking } from './lib/tracking';
 import './styles.css';
+
+initTracking();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
