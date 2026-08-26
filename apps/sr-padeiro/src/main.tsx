dff67d0747ddd899c677a15b70aabc6320ad7fca
CodexSandboxOffline <CodexSandboxOffline@DESKTOP-G1CGHM8.prohospital.local>
2026-08-26 09:20:09 -0300

    feat(srp): close acquisition and activation attribution

--- apps/sr-padeiro/src/main.tsx
+++ apps/sr-padeiro/src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MarketingConsent from './MarketingConsent';
 import { initTracking } from './lib/tracking';
 import './styles.css';
 import './workspace.css';
 import './landing-v2.css';
+import './marketing-consent.css';
 
 initTracking();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <MarketingConsent />
   </React.StrictMode>,
 );
